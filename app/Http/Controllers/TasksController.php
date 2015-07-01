27957bbe89b6350693bab8f808eb84d0abe99474
11 Digit Labs <labs@11dig.it>
2015-07-01 16:49:32 +0300

    Get all tasks ordered by created_at desc, since new tasks are added on the beginning of the list 

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -16,7 +16,7 @@ class TasksController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all();
+        $tasks = Task::orderBy( 'created_at', 'desc' )->get();
         return $tasks;
     }
 
